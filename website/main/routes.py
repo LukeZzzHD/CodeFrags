c5f4817ef9465a303e3889e65b3e25e31de806f1
Schwab Lukas <aogq@mediait.ch>
2018-11-13 13:24:37 +0100

    Commit new project structure
    new

---
+++ website/main/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template, request, Blueprint
+from website.models import Post
+
+main = Blueprint('main', __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return render_template('home.html', title="Home")
