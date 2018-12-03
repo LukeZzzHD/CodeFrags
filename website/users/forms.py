7d3dccb1f9ecbe3fddef3e308e2a5f669db623df
Lukas Schwab <lukas.schwab@tamedia.ch>
2018-12-03 12:49:25 +0100

    Prism completly working
    :)

--- website/users/forms.py
+++ website/users/forms.py
@@ -75,7 +75,7 @@ class ResetPasswordForm(FlaskForm):
 
 class NewPostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=3, max=30)])
-    language = SelectField('Programming language', validators=[DataRequired()], choices=[('javascript', 'Javascript'), ('csharp', 'C#'), ('cplusplus', 'C++'), ('python', 'Python'), ('c', 'C')])
+    language = SelectField('Programming language', validators=[DataRequired()], choices=[('java', 'Java'), ('javascript', 'Javascript'), ('csharp', 'C#'), ('cpp', 'C++'), ('python', 'Python'), ('c', 'C'), ('markup', 'HTML'), ('ruby', 'Ruby'), ('sql', 'SQL'), ('css', 'CSS'), ('go', 'GO'), ('php', 'PHP'), ('jsx', 'JSX')])
     content = TextAreaField('Your Code', validators=[DataRequired()])
     description = TextAreaField('Post description', validators=[DataRequired()])
     submit = SubmitField('Post')
