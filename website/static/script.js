eb38c75a67586eded4ab871d7b7f789ade894483
Lukas Schwab <lukas.schwab@bbcag.ch>
2018-12-03 17:18:23 +0100

    c
    c

--- website/static/script.js
+++ website/static/script.js
@@ -3,9 +3,4 @@ Prism.plugins.NormalizeWhitespace.setDefaults({
 	'remove-indent': true,
 	'left-trim': true,
 	'right-trim': true,
-	/*'break-lines': 80,
-	'indent': 2,
-	'remove-initial-line-feed': false,
-	'tabs-to-spaces': 4,
-	'spaces-to-tabs': 4*/
 });
